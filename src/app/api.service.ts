6e6a15da5c2e1ad3d1ed347fd968928d0c1855b1
Pocento <cristiancid88@gmail.com>
2021-11-10 04:34:09 -0300

    mapa api v1.6

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,13 +7,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
+  
   httpOptions={
     headers: new HttpHeaders({
       'Content-Type':'application/json',
       'Access-Control-Allow-Origin':'**'
     })
   }
+
   constructor( private http: HttpClient) { }
+
   apiURL='https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCwYek7uo-LI6LT9qYWLpfnpELnAXrPPLY&address=';
 
   getDireccion(direccion:string): Observable<any>{
